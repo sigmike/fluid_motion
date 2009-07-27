b6a6297a154f861de286b99de3de47da666d4050
piglop <mike@lepton.fr>
2009-07-28 00:52:52 +0200

    better triangle colors

--- lesson5.c
+++ lesson5.c
@@ -121,33 +121,31 @@ void DrawGLScene()
   glColor3f(1.0f,0.0f,0.0f);			// Set The Color To Red
   glVertex3f(0.0f, 1.0f, 0.0f);		        // Top of triangle (front)
   glColor3f(0.0f,1.0f,0.0f);			// Set The Color To Green
-  glVertex3f(-1.0f,-1.0f, 1.0f);		// left of triangle (front)
-  glColor3f(0.0f,0.0f,1.0f);			// Set The Color To Blue
-  glVertex3f(1.0f,-1.0f, 1.0f);		        // right of traingle (front)	
+  glVertex3f(-0.1f,0.0f, 0.1f);		// left of triangle (front)
+  glVertex3f(0.1f,0.0f, 0.1f);		        // right of traingle (front)	
 
   // right face of pyramid
   glColor3f(1.0f,0.0f,0.0f);			// Red
   glVertex3f( 0.0f, 1.0f, 0.0f);		// Top Of Triangle (Right)
-  glColor3f(0.0f,0.0f,1.0f);			// Blue
-  glVertex3f( 1.0f,-1.0f, 1.0f);		// Left Of Triangle (Right)
   glColor3f(0.0f,1.0f,0.0f);			// Green
-  glVertex3f( 1.0f,-1.0f, -1.0f);		// Right Of Triangle (Right)
+  glVertex3f( 0.1f,0.0f, 0.1f);		// Left Of Triangle (Right)
+  glColor3f(0.0f,0.0f,1.0f);			// Blue
+  glVertex3f( 0.1f,0.0f, -0.1f);		// Right Of Triangle (Right)
 
   // back face of pyramid
   glColor3f(1.0f,0.0f,0.0f);			// Red
   glVertex3f( 0.0f, 1.0f, 0.0f);		// Top Of Triangle (Back)
-  glColor3f(0.0f,1.0f,0.0f);			// Green
-  glVertex3f( 1.0f,-1.0f, -1.0f);		// Left Of Triangle (Back)
   glColor3f(0.0f,0.0f,1.0f);			// Blue
-  glVertex3f(-1.0f,-1.0f, -1.0f);		// Right Of Triangle (Back)
+  glVertex3f( 0.1f,0.0f, -0.1f);		// Left Of Triangle (Back)
+  glVertex3f(-0.1f,0.0f, -0.1f);		// Right Of Triangle (Back)
 
   // left face of pyramid.
   glColor3f(1.0f,0.0f,0.0f);			// Red
   glVertex3f( 0.0f, 1.0f, 0.0f);		// Top Of Triangle (Left)
   glColor3f(0.0f,0.0f,1.0f);			// Blue
-  glVertex3f(-1.0f,-1.0f,-1.0f);		// Left Of Triangle (Left)
+  glVertex3f(-0.1f,0.0f,-0.1f);		// Left Of Triangle (Left)
   glColor3f(0.0f,1.0f,0.0f);			// Green
-  glVertex3f(-1.0f,-1.0f, 1.0f);		// Right Of Triangle (Left)
+  glVertex3f(-0.1f,0.0f, 0.1f);		// Right Of Triangle (Left)
 
   glEnd();					// Done Drawing The Pyramid
 
